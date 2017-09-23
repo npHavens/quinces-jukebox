2b0c7f0b1404db8e007538d1a7c877f9d66b9285
Nick Havens <npahavens@gmail.com>
2017-09-23 15:44:45 -0500

    add implementation of getTrackSearchResults helper

--- helpers/spotifyHelpers.js
+++ helpers/spotifyHelpers.js
@@ -1,5 +1,6 @@
 const credentials = require('../env/credentials.js');
 const request = require('request');
+const Promise = require('bluebird');
 
 const authOptions = {
   url: 'https://accounts.spotify.com/api/token',
@@ -12,8 +13,23 @@ const authOptions = {
   json: true
 };
 
-const authorizedRequest = (callback) => {}
-
 exports.getTrackSearchResults = (queryString) => {
-
+  return new Promise((resolve, reject) => {
+    request.post(authOptions, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const token = body.access_token;
+        const options = {
+          url: `https://api.spotify.com/v1/search?q=${queryString}&type=track&market=US&limit=10`,
+          headers: {'Authorization': 'Bearer ' + token},
+          json: true
+        };
+        request.get(options, (error, response, body) => {
+          if (error) {
+            reject(error);
+          }
+          resolve(body);
+        });
+      }
+    });
+  });
 }
