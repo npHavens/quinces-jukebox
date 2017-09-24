8570f56280a8c61f3735c42a77b346f769b0a833
Nick Havens <npahavens@gmail.com>
2017-09-24 18:04:38 -0500

    add code for handleHostLogin helper

--- helpers/spotifyHelpers.js
+++ helpers/spotifyHelpers.js
@@ -13,6 +13,16 @@ const authOptions = {
   json: true
 };
 
+const generateRandomString = (length) => {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
 exports.getTrackSearchResults = (queryString) => {
   return new Promise((resolve, reject) => {
     request.post(authOptions, (error, response, body) => {
@@ -35,7 +45,19 @@ exports.getTrackSearchResults = (queryString) => {
 };
 
 exports.handleHostLogin = (req, res) => {
+  const state = generateRandomString(16);
+  const scope = 'user-read-private user-read-email user-read-playback-state';
 
+  res.cookie('spotify_auth_state', state);
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: credentials.client_id,
+      scope: scope,
+      redirect_uri: 'http://localhost:3000/callback',
+      state: state
+    }));
 };
 
 exports.redirectAfterLogin = (req, response) => {
