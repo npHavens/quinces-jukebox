715866f91df20b762e184042c62b8a21c6843be4
Nick Havens <npahavens@gmail.com>
2017-09-25 00:13:21 -0500

    add link to host login on nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,7 @@ const Navbar = () => (
       <li><Link to="/signup">Sign Up</Link></li>
       <li><Link to="/login">Login</Link></li>
       <li><Link to="/search">Search</Link></li>
+      <li><a href="/hostLogin">Login as Host</a></li>
     </ul>
   </div>
 )
