1c28f274f5e1a96d14973cdea2d42305824acfd8
Jessica D'Andrea <jessicad@jessicas-air.attlocal.net>
2017-09-27 23:41:42 -0500

    Changed hostLogin href

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => (
       <li><Link to="/signup">Sign Up</Link></li>
       <li><Link to="/login">Login</Link></li>
       <li><Link to="/search">Search</Link></li>
-      <li><a href="/hostLogin">Login as Host</a></li>
+      <li><a href="http://localhost:3000/hostLogin">Login as Host</a></li>
     </ul>
   </div>
 )
